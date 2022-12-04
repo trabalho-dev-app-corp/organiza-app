7ea0dbf728547848812563ea65b8338a574954cb
Lucas Costa <lnatanfrancacosta@gmail.com>
2022-12-04 18:02:49 -0300

    projeto inicializado

---
+++ src/main/java/com/devappcorp/organiza/organizaapp/OrganizaAppApplication.java
@@ -0,0 +1,13 @@
+package com.devappcorp.organiza.organizaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devappcorp/organiza/organizaapp/OrganizaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devappcorp.organiza.organizaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
