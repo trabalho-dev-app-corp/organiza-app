03fbb915cba7ca99dbcc75e9973aa5d5bf91f19d
Lucas Costa <lnatanfrancacosta@gmail.com>
2022-12-06 20:37:39 -0300

    add classes de domínio TipoUsuario e Usuario

---
+++ src/main/java/com/devappcorp/organiza/organizaapp/domain/model/TipoUsuario.java
@@ -0,0 +1,27 @@
+package com.devappcorp.organiza.organizaapp.domain.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class TipoUsuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    
+    @ManyToMany(mappedBy = "tiposUsuario")
+    private List<Usuario> usuarios = new ArrayList<>();
+}
--- src/main/java/com/devappcorp/organiza/organizaapp/domain/model/Usuario.java
+++ src/main/java/com/devappcorp/organiza/organizaapp/domain/model/Usuario.java
@@ -1,5 +1,43 @@
 package com.devappcorp.organiza.organizaapp.domain.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Usuario {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String nome;
+
+    private String email;
+
+    private String afiliacao;
+
+    @ManyToMany 
+    @JoinTable( 
+        name = "usarios_tipos", 
+        joinColumns = @JoinColumn(
+          name = "usuario_id", referencedColumnName = "id"), 
+        inverseJoinColumns = @JoinColumn(
+          name = "tipo_usuario_id", referencedColumnName = "id"))
+    private List<TipoUsuario> tiposUsuario = new ArrayList<>();
 }
