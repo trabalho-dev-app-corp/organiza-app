aaac78be5db395a70a4f22b6c550df395d5a2fba
Lucas Costa <lnatanfrancacosta@gmail.com>
2022-12-06 09:36:21 -0300

    add métodos de busca por id e inclusão

--- src/main/java/com/devappcorp/organiza/organizaapp/api/controller/EventoController.java
+++ src/main/java/com/devappcorp/organiza/organizaapp/api/controller/EventoController.java
@@ -3,7 +3,12 @@ package com.devappcorp.organiza.organizaapp.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +25,22 @@ public class EventoController {
     @GetMapping
     public List<Evento> listar() {
         return eventoRepository.findAll();
+    }
+
+    @GetMapping("/{eventoId}")
+    public ResponseEntity<Evento> buscar(@PathVariable("eventoId") Long eventoId) {
+        Evento evento = eventoRepository.findById(eventoId).get();
+
+        if (evento != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(evento);
+        } 
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Evento> adicionar(@RequestBody Evento evento) {
+        eventoRepository.save(evento);
+        return ResponseEntity.status(HttpStatus.CREATED).body(evento);
     }
 }
