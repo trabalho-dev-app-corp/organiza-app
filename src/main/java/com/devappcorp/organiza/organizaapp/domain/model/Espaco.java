2bf824363a64bd7a784d65b47f2eaaf03a686511
Daniel Neves <dbenditoneves@gmail.com>
2022-12-10 12:22:57 -0300

    Feito CRUD de espaco e finalizado o de Evento

--- src/main/java/com/devappcorp/organiza/organizaapp/domain/model/Espaco.java
+++ src/main/java/com/devappcorp/organiza/organizaapp/domain/model/Espaco.java
@@ -1,5 +1,27 @@
 package com.devappcorp.organiza.organizaapp.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Espaco {
+        
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
     
+    private String localizacao;
+    
+    private int capacidade;
+
+    private String[] recursos;
 }
---
+++ src/main/java/com/devappcorp/organiza/organizaapp/domain/repository/EspacoRepository.java
@@ -0,0 +1,9 @@
+package com.devappcorp.organiza.organizaapp.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devappcorp.organiza.organizaapp.domain.model.Espaco;
+
+public interface EspacoRepository extends JpaRepository<Espaco, Long>{
+    
+}
