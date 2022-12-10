26d54fb45627e9b6dbc852ec917db025f99208f3
Vitor Prado <vitor.prado@primeup.com.br>
2022-12-10 16:47:37 -0300

    criação da estrutura do controller, preenchimento do model de atividades e criação do repository

--- src/main/java/com/devappcorp/organiza/organizaapp/domain/model/Atividade.java
+++ src/main/java/com/devappcorp/organiza/organizaapp/domain/model/Atividade.java
@@ -1,5 +1,37 @@
 package com.devappcorp.organiza.organizaapp.domain.model;
 
+import java.sql.Date;
+import java.sql.Time;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+
+
 public class Atividade {
+        
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String tipo;
     
+    private String descricao;
+    
+    private Date data;
+
+    private Time horarioInicial;
+
+    private Time horarioFinal;
 }
