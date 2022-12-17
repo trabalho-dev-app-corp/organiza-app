b6088021bcf9c053783e0d7414381976000bdd3c
walber-fac <74104683+walber-fac@users.noreply.github.com>
2022-12-17 13:13:41 -0300

    put e delete no evento controller - tentativa

--- src/main/java/com/devappcorp/organiza/organizaapp/api/controller/EdicaoController.java
+++ src/main/java/com/devappcorp/organiza/organizaapp/api/controller/EdicaoController.java
@@ -1,14 +1,17 @@
-
 package com.devappcorp.organiza.organizaapp.api.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +49,28 @@ public class EdicaoController {
         edicaoRepository.save(edicao);
         return ResponseEntity.status(HttpStatus.CREATED).body(edicao);
     }
-}
+    @PutMapping("/{edicaoId}")
+    public ResponseEntity<Edicao> editar(@PathVariable("edicaoId") Long edicaoId, @RequestBody Edicao edicaoInfo) {
+        Edicao edicao = edicaoRepository.findById(edicaoId).get();
 
+        edicao.setNumero(edicaoInfo.getNumero());
+        edicao.setCidade(edicaoInfo.getCidade());
+        final Edicao edicaoAtualizado = edicaoRepository.save(edicao);
+        return ResponseEntity.ok(edicaoAtualizado);
+    }
+
+    @DeleteMapping("/{edicaoId}")
+    public Map<String, Boolean> deletar(@PathVariable("edicaoId") Long edicaoId) {
+        Edicao edicao = edicaoRepository.findById(edicaoId).get();
+
+        edicaoRepository.delete(edicao);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deletado", Boolean.TRUE);
+        return response;
+    }
+
+
+
+
+
+}
